d14ae79733f11062c97b20e20c55c94dd898725a
Thomas Morgan <thomasmorgan.webdev@gmail.com>
2021-12-08 21:27:00 +0000

    v1.0.4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+var DOMParser = require('xmldom').DOMParser;
+
 module.exports = function Input(type, placeholder = null) {
 
   const input = `<input type=${type} placeholder=${placeholder}} />`;
 
-  const parser = new DOMParser();
-
-  return parser.parseFromString(input, "text/html");
+  return new DOMParser().parseFromString(input, "text/html");
 };
